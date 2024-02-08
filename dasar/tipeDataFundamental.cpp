ef5cc5d614a59c1cdc0fd06bfeaf26d1105ecf21
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-09 01:14:50 +0700

    tipe data fundamental

---
+++ dasar/tipeDataFundamental.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // bilangan bulat
+    int a; // 32 bit
+    long b; // 32 bit
+    short c; // 8 bit
+
+    //bilangan decimal
+    float d;
+    double e;
+
+    // character
+    char f ='f';
+
+    //bolean
+    bool g; //true/false
+}
