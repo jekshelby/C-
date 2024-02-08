a47d3c2a3f1f36835495f935170fd166b10c2d90
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-09 01:02:06 +0700

    deklarasi dan cin(input)

---
+++ dasar/deklarasiDanCin.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    cout << "angka variable a = " << a << endl;
+
+    int b;
+    cout << "Masukkan Nilai ";
+    cin >> b;
+    cout << "Nilai yang dimasukkan adalah " << b;
+    
+    return 0;
+}
