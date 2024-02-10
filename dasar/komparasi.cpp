2f7231ac3bdeddc00f6fe98fc2b1490316c2ea36
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-10 15:52:53 +0700

    komparasi

---
+++ dasar/komparasi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = 2;
+
+    bool hasil1, hasil2;
+
+    // komparasi / relation expression
+
+    // sebanding
+    hasil1 = a == b;
+
+    // sebanding
+    hasil2 = a != b;
+
+    // kurang dari
+    hasil1 = a < b;
+
+    // lebih dari
+    hasil1 = a > b;
+    
+    // kurang dari samadengan
+    hasil1 = a <= b;
+
+    // lebih dari samadengan
+    hasil1 = a >= b;
+
+    // cout << hasil1 << endl;
+    // cout << hasil2 << endl;
+
+    return 0;
+}
