ae86e690cabc8d576c0ed81bb06d75b94f8ae0eb
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-10 16:08:52 +0700

    if statement

---
+++ dasar/if.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cout << "masukkan angka : ";
+    cin >> a ;
+
+    if ( a ) {
+        cout << "hai" << endl;
+    }
+
+    cout << "selesai";
+
+    return 0;
+} 
+
