961607c10f7fb2a89ae23ec17ab897583f9fd989
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-11 21:48:11 +0700

    function bawaan library

---
+++ function/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cout << "Masukkan Akar : ";
+    cin >> x;
+
+    cout << "Akarnya adalah : " << sqrt(x);
+    return 0;
+}   
